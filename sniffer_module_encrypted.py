9f42c5f7a76dd965466438808b235eefe628b4ea
Cristóbal  Nilo <cristobal.nilo@gmail.com>
2022-12-11 00:49:38 -0300

    Minor fixes

--- sniffer_module_encrypted.py
+++ sniffer_module_encrypted.py
@@ -17,16 +17,16 @@ def sniff(interface):
     print("mensaje: " + dhlib.dhDecrypt(str(mensaje),my_full))
 
     print("Sniffer:" + str(my_full))
-    #scapy.sniff(iface=interface, store=False, prn=sniffed_packet)
+    scapy.sniff(iface=interface, store=False, prn=sniffed_packet)
 
 
 
-#def sniffed_packet(packet):
-    #if type(packet.payload.payload.payload) == scapy.packet.Raw:
-    #    payload = bytes(packet.payload.payload.payload)
-    #    if payload[0:6] == b'sensor':
-    #        temp = float(payload[6:-2].decode())
-    #        print(temp)
+def sniffed_packet(packet):
+    if type(packet.payload.payload.payload) == scapy.packet.Raw:
+        payload = bytes(packet.payload.payload.payload)
+        if payload[0:6] == b'sensor':
+            temp = float(payload[6:-2].decode())
+            print(temp)
 
 
 def main():
